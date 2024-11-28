164d5c76baeccc5dc42acad2b3b6555a18ff01f3
Moritz Tim W <90388353+moritztim@users.noreply.github.com>
2024-11-28 15:59:04 +0100

    add source

--- src/@types/lexical.d.ts
+++ src/@types/lexical.d.ts
@@ -1,3 +1,4 @@
+// https://github.com/forhappy/editor/blob/0958d2adc9950556eba02a0e2fb87dee748afa0d/src/%40types/lexical/index.d.ts
 import 'lexical'
 import { EditorConfig as LexicalEditorConfig, EditorThemeClasses as LexicalEditorThemeClasses } from 'lexical'
 
